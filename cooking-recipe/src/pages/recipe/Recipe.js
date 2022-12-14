876233d0cd8b17413141339f6aa1e11b9dbb29df
nisha-07 <nishasataki@gmail.com>
2022-12-14 17:41:51 +0530

    CR: Component to show recipe details

--- cooking-recipe/src/pages/recipe/Recipe.js
+++ cooking-recipe/src/pages/recipe/Recipe.js
@@ -1,9 +1,28 @@
 import "./Recipe.css";
 
+import { Audio } from "react-loader-spinner";
+import { useFetch } from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+
 const Recipe = () => {
+    const { id } = useParams();
+    const { data: recipe, isLoading, error } = useFetch(`http://localhost:3000/recipes/${id}`)
+
     return (
         <div>
-            Recipe page
+            {isLoading && <div className="loading">
+                <Audio
+                    height="80"
+                    color="#58249c"
+                /></div>}
+            {error && <div className="error">{error}</div>}
+            {recipe &&
+                <div className="recipe">
+                    <h3 className="page-title">{recipe.title}</h3>
+                    <p>Time {recipe.cookingTime} to cook.</p>
+                    <ul>{recipe.ingredients.map((ing) => <li key={ing}>{ing}</li>)}</ul>
+                    <p className="method">{recipe.method}</p>
+                </div>}
         </div>
     )
 }
