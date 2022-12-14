b2b627e880f6853b9e68ea1bb83f7bf2593b09a8
nisha-07 <nishasataki@gmail.com>
2022-12-14 11:15:31 +0530

    CR: Add router package
    Add routes for different pages

--- cooking-recipe/src/App.js
+++ cooking-recipe/src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom"
+
+import Create from './pages/create/Create';
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+
 function App() {
   return (
     <div className='d-flex'>
-      Cooking Recipe!
-    </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="*">
+            <Redirect path="/" />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div >
   );
 }
 
