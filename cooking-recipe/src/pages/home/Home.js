2f23b50b1d811fbf900cec8c2031748cdadb17d0
nisha-07 <nishasataki@gmail.com>
2022-12-14 12:23:35 +0530

    CR: Create a cusotm hook to fetch data
    Add package for loader
    Show data on home page
    Styling components

--- cooking-recipe/src/pages/home/Home.js
+++ cooking-recipe/src/pages/home/Home.js
@@ -1,9 +1,23 @@
 import "./Home.css";
 
+import { Audio } from 'react-loader-spinner'
+import { useFetch } from "../../hooks/useFetch";
+
 const Home = () => {
+    const { data: recipes, isLoading, error } = useFetch("http://localhost:3000/recipes");
+
     return (
-        <div>
-            Home page
+        <div className="home">
+            {isLoading && <div className="loading"><Audio
+                height="80"
+                color="#58249c"
+            /></div>}
+            {error && <div className="error">{error}</div>}
+            {recipes && recipes.map((recipe => (
+                <div key={recipe.id}>
+                    <h4>{recipe.title}</h4>
+                </div>
+            )))}
         </div>
     )
 }
