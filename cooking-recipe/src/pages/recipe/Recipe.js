d10b1789b4d5aacd65912bfb9bf2149aa11e50a9
nisha-07 <nishasataki@gmail.com>
2022-12-16 11:04:41 +0530

    Firebase: Fetch recipe detail using firebase

--- cooking-recipe/src/pages/recipe/Recipe.js
+++ cooking-recipe/src/pages/recipe/Recipe.js
@@ -1,12 +1,33 @@
 import "./Recipe.css";
 
 import { Audio } from "react-loader-spinner";
-import { useFetch } from "../../hooks/useFetch";
+
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { projectFirebasestore } from "../../firebase/config";
 
 const Recipe = () => {
-    const { id } = useParams();
-    const { data: recipe, isLoading, error } = useFetch(`http://localhost:3000/recipes/${id}`)
+    const { id } = useParams()
+
+    const [recipe, setRecipe] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true)
+
+        projectFirebasestore.collection("recipes").doc(id).get().then((doc) => {
+            if (doc.exists) {
+                setIsLoading(false)
+                setRecipe(doc.data())
+            }
+            else {
+                setIsLoading(false)
+                setError("Could not find the recipe")
+            }
+        })
+
+    }, [])
 
     return (
         <div>
